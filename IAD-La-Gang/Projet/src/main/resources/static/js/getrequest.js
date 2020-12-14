3be30f6eeacad54e14b01b80d9789e01dfddff2b
Tanguy Benard <t-benard@outlook.fr>
2020-12-14 20:30:00 +0100

    Refactor folders

---
+++ IAD-La-Gang/Projet/src/main/resources/static/js/getrequest.js
@@ -0,0 +1,150 @@
+$( document ).ready(function() {
+
+
+
+
+
+	// GET REQUEST
+	$("#getAllCustomerId").click(function(event){
+		event.preventDefault();
+		ajaxGet();
+	});
+	var airport = null;
+	var vol = null;
+
+	loadVol();
+	loadCustomerDropdown();
+
+
+	
+	// DO GET
+	function ajaxGet(){
+		$.ajax({
+			type : "GET",
+			url : window.location + "api/customer/all",
+			success: function(result){
+				if(result.status == "Done"){
+					$('#getResultDiv ul').empty();
+					$.each(result.data, function(i, customer){
+						var customer = "- Customer with Id = " + i + ", firstname = " + customer.firstname + ", lastName = " + customer.lastname + "<br>";
+						$('#getResultDiv .list-group').append(customer)
+			        });
+					console.log("Success: ", result);
+				}else{
+					$("#getResultDiv").html("<strong>Error</strong>");
+					console.log("Fail: ", result);
+				}
+			},
+			error : function(e) {
+				$("#getResultDiv").html("<strong>Error</strong>");
+				console.log("ERROR: ", e);
+			}
+		});	
+	}
+
+	function loadAirport(){
+		$.ajax({
+			type : "GET",
+			url : window.location + "api/aeroport/all",
+			success: function(json) {
+				airport = json.data;
+			}
+		});
+	}
+
+	function loadCustomerDropdown(){
+		$.ajax({
+			type : "GET",
+			url : window.location + "api/customer/all",
+			success: function(json) {
+				$.each(json.data, function (i, value) {
+					var elements = document.getElementsByName('customerDropdown');
+					for (var i = 0; i < elements.length; i++) {
+						$('<option></option>', {text: value.email}).attr('value', value).appendTo(elements[i]);
+					}
+					$('<option></option>', {text: value.email}).attr('value', value).appendTo($("#customerToDisplay"));
+				});
+			}
+		});
+	}
+
+	function loadVol(){
+		loadAirport();
+		$.ajax({
+			type: "GET",
+			url: window.location + "api/vol/all",
+			success: function (json) {
+				$.each(json.data, function (i, value) {
+					vol = json.data;
+					$('<tr scope="col"></tr>').append(
+						$('<td class="text-center"></td>').text(airport[value.depart].code + " (" + airport[value.depart].nom + ")"),
+						$('<td class="text-center"></td>').text(airport[value.arrive].code + " (" + airport[value.arrive].nom + ")"),
+						$('<td class="text-center"></td>').text(value.date),
+						$('<td class="text-center"></td>').text(value.prix + "€"),
+						$('<td class="text-center"></td>').text(value.disponibilite),
+						$('<td></td>').append($('<select name="customerDropdown" class="form-control"></select>').attr("form", i)),
+						$('<td></td>').append($('<button class="btn btn-primary" type="submit"></button>').text('Reserver').attr({ form:i,  name:"ticketForm" })))
+					.appendTo('#volTable');
+				});
+			}
+		});
+	}
+
+	$("#customerForm").submit(function(event) {
+		// Prevent the form from submitting via the browser.
+		setTimeout(function () {
+			console.log('test');
+			$.each(document.getElementsByName("customerDropdown"), function () {
+				$(this).empty()
+				$("#ticketTable").empty()
+			});
+
+			event.preventDefault();
+			loadCustomerDropdown();
+		},100);
+	});
+
+
+
+
+	$("#displayTicketForm").submit(function(event) {
+		// Prevent the form from submitting via the browser.
+		event.preventDefault();
+		loadCustomerTicket(	$("#customerToDisplay").prop('selectedIndex'));
+
+	});
+
+
+
+
+	function loadCustomerTicket(customer){
+		$.ajax({
+			type: "POST",
+			url: window.location + "api/ticket/bycustomer",
+			data: { customer: parseInt(customer) },
+			success: function (json) {
+				console.log(json.data);
+
+				$.each(json.data, function (i, value) {
+					var depart = parseInt(vol[value.vol].depart);
+					var arrive = parseInt(vol[value.vol].arrive);
+					console.log(airport);
+					console.log(depart);
+					console.log(airport[depart].code);
+					$('<tr scope="col"></tr>').append(
+						$('<td class="text-center"></td>').text(airport[depart].code + " (" + airport[depart].nom + ")"),
+						$('<td class="text-center"></td>').text(airport[arrive].code + " (" + airport[arrive].nom + ")"),
+						$('<td class="text-center"></td>').text(vol[value.vol].date),
+						$('<td class="text-center"></td>').text(vol[value.vol].prix+ "€"))
+						.appendTo('#ticketTable');
+				});
+			},
+			error: function (e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
+	}
+	// DO POST
+
+})
---
+++ IAD-La-Gang/Projet/src/main/resources/static/js/postrequest.js
@@ -0,0 +1,96 @@
+$( document ).ready(function() {
+	
+	// SUBMIT FORM
+    $("#customerForm").submit(function(event) {
+		// Prevent the form from submitting via the browser.
+		event.preventDefault();
+		customerPost();
+	});
+    
+    
+    function customerPost(){
+    	
+    	// PREPARE FORM DATA
+    	var formData = {
+    		email : $("#email").val(),
+    	}
+    	
+    	// DO POST
+    	$.ajax({
+			type : "POST",
+			contentType : "application/json",
+			url : window.location + "api/customer/save",
+			data : JSON.stringify(formData),
+			dataType : 'json',
+			success : function(result) {
+				if(result.status == "Done"){
+					$("#postResultDiv").html("<p style='background-color:#7FA7B0; color:#ffffff; padding:20px 20px 20px 20px'>" +
+												"Votre email a bien été enregistré <br>" +
+												"Votre email = " +
+												result.data.email);
+				}else{
+					$("#postResultDiv").html("<strong>Error</strong>");
+				}
+				console.log(result);
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
+    	
+    	// Reset FormData after Posting
+    	resetData();
+
+    }
+    
+    function resetData(){
+    	$("#email").val("");
+    }
+
+	function ticketPost(customer, vol) {
+    	alert(vol);
+
+		// PREPARE FORM DATA
+		var formData = {
+			customer: customer,
+			vol: vol,
+		}
+
+		// DO POST
+		$.ajax({
+			type: "POST",
+			contentType: "application/json",
+			url: window.location + "api/ticket/save",
+			data: JSON.stringify(formData),
+			dataType: 'json',
+			success: function (result) {
+				if (result.status == "Done") {
+					alert('Votre reservation a bien été enregistré')
+					$("#postResultDiv").html("<p style='background-color:#7FA7B0; color:white; padding:20px 20px 20px 20px'>" +
+						"Votre email a bien été enregistré <br>")
+
+				} else {
+					$("#postResultDiv").html("<strong>Error</strong>");
+				}
+				console.log(result);
+			},
+			error: function (e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
+	}
+
+	setTimeout(function () {
+		$.each(document.getElementsByName("ticketForm"), function () {
+			$(this).click(function(event) {
+
+				// Prevent the form from submitting via the browser.
+				event.preventDefault();
+				ticketPost($(this).closest("tr").find("select").prop('selectedIndex'), $(this).attr('form'));
+			});
+
+		});
+	},100);
+})
